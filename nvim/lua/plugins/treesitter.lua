cdf8b40fe00d936ee9213e0e6282608ddb70624b
Reqox <anovikova999666@gmail.com>
2026-02-22 13:56:15 +0300

    02-22

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,39 +1,37 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	event = { "BufReadPost", "BufNewFile" },
-	opts = {
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		indent = { enable = true },
-		ensure_installed = {
-			-- Frontend & Backend
-			"javascript",
-			"typescript",
-			"tsx",
-			"jsx",
-			"vue",
-			"html",
-			"css",
-			"scss",
-			"sql",
-			-- Other
-			"lua",
-			"vim",
-			"vimdoc",
-			"json",
-			"yaml",
-			"markdown",
-			"markdown_inline",
-			"bash",
-			"dockerfile",
-			"gitignore",
-
-			"python",
-			"c",
-			"cpp",
-		},
-	},
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+			ensure_installed = {
+				"javascript",
+				"typescript",
+				"tsx",
+				"vue",
+				"html",
+				"css",
+				"scss",
+				"sql",
+				"lua",
+				"vim",
+				"vimdoc",
+				"json",
+				"yaml",
+				"markdown",
+				"markdown_inline",
+				"bash",
+				"dockerfile",
+				"gitignore",
+				"python",
+				"c",
+				"cpp",
+			},
+		})
+	end,
 }
